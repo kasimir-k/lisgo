7802c7d01e02c4212484e75a2e7a55adc33f2fbd
Kasimir Pihlasviita <kasimir@pihlasviita.fi>
2026-03-18 16:36:42 +0200

    Formatting

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2";
 import { solidStart } from "@solidjs/start/config";
 
 export default defineConfig({
-  plugins: [solidStart(),
+  plugins: [
+    solidStart(),
     nitro()
   ]
 });
